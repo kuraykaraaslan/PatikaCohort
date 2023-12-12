ec3a002d5d30f1b49bf1b8173b2b05ca4ac83146
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2023-12-12 16:41:09 +0300

    Week One

--- WeekOnePowerOfTwoBackward/src/Main.java
+++ WeekOnePowerOfTwoBackward/src/Main.java
@@ -10,16 +10,18 @@ public class Main {
         int number = scanner.nextInt();
 
         int powerOfTwo = 0;
+        int powerOfFour = 0;
+        int powerOfFive = 0;
 
-        // Check if the number is a power of 2
+        // Check if the number is a power of 2, 4, or 5
         while (true)
         {
             // Check if number is greater than 2 raised to the powerOfTwo
-            if (number > (2 ^ powerOfTwo))
+            if (number > Math.pow(2, powerOfTwo))
             {
                 // Check if number is not equal to 2 raised to the powerOfTwo + 1
                 // Determine if the number is indeed a power of two
-                if (number != (2 ^ (powerOfTwo + 1)))
+                if (number != Math.pow(2, powerOfTwo + 1))
                 {
                     break;
                 }
@@ -27,7 +29,32 @@ public class Main {
                 {
                     powerOfTwo++;
                 }
-
+            }
+            else if (number > Math.pow(4, powerOfFour))
+            {
+                // Check if number is not equal to 4 raised to the powerOfFour + 1
+                // Determine if the number is indeed a power of four
+                if (number != Math.pow(4, powerOfFour + 1))
+                {
+                    break;
+                }
+                else
+                {
+                    powerOfFour++;
+                }
+            }
+            else if (number > Math.pow(5, powerOfFive))
+            {
+                // Check if number is not equal to 5 raised to the powerOfFive + 1
+                // Determine if the number is indeed a power of five
+                if (number != Math.pow(5, powerOfFive + 1))
+                {
+                    break;
+                }
+                else
+                {
+                    powerOfFive++;
+                }
             }
             else
             {
@@ -37,6 +64,7 @@ public class Main {
 
         // Print the result
         System.out.println("The number " + number + " is " + powerOfTwo + " power of two");
+        System.out.println("The number " + number + " is " + powerOfFour + " power of four");
+        System.out.println("The number " + number + " is " + powerOfFive + " power of five");
     }
 }
-
