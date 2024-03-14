d0884065b2194ec383894d40ceba75d9a60b85a9
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2024-03-14 23:45:40 +0300

    WeekFourteen

---
+++ WeekFourteenAnotherPage/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event){
+    event.preventDefault(); // Prevents the default form submission action
+    alert('Thank you for reaching out. I will get back to you as soon as possible!');
+});
